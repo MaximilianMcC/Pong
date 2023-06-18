87fe8b38795d838df57a51bf224636caa2d10ec6
MaximilianMcC <max@maximilian.co.nz>
2023-06-19 07:33:19 +1000

    Added paddles with window collision

--- Game.cs
+++ Game.cs
@@ -5,6 +5,7 @@ using SFML.Window;
 class Game
 {
 	public RenderWindow Window;
+	public float DeltaTime;
 	
 	public void Run()
 	{
@@ -13,22 +14,36 @@ class Game
 		Window.SetFramerateLimit(60);
 		Window.Closed += (sender, e) => Window.Close();
 
-
 		// Create the delta time clock
 		Clock deltaTimeClock = new Clock();
+
+
+		// Create both the left and right paddles
+		Paddle leftPaddle = new Paddle(PaddleType.LEFT, Color.Blue, this);
+		Paddle rightPaddle = new Paddle(PaddleType.RIGHT, Color.Red, this);
+
 
 		// Game
 		while (Window.IsOpen)
 		{
 			// Events and whatnot
 			Window.DispatchEvents();
+			DeltaTime = deltaTimeClock.Restart().AsSeconds();
 
+
+			// Update the paddles
+			leftPaddle.Update();
+			rightPaddle.Update();
 
 
 
 			// Clear the window
 			Window.Clear(Color.Magenta);
 			
+			// Draw the paddles
+			leftPaddle.Render();
+			rightPaddle.Render();
+
 			// Show the new frame
 			Window.Display();
 		}
---
+++ Paddle.cs
@@ -0,0 +1,95 @@
+using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+
+class Paddle
+{
+	private Game game;
+	private RectangleShape sprite;
+	private float position;
+	private float x;
+	private float speed = 727f;
+	private PaddleType paddleType;
+	private float width = 25f;
+	private float height = 200f;
+
+	// Create a new paddle
+	public Paddle(PaddleType paddleType, Color color, Game game)
+	{
+		this.game = game;
+		this.paddleType = paddleType;
+
+		// Create the sprite
+		this.sprite = new RectangleShape(new Vector2f(width, height));
+
+		// Set the position
+		if (paddleType == PaddleType.LEFT) this.x = 0;
+		else if (paddleType == PaddleType.RIGHT) this.x = game.Window.Size.X - width;
+	}
+
+	public void Update()
+	{
+		Movement();
+	}
+
+	public void Render()
+	{
+		game.Window.Draw(sprite);
+	}
+
+
+
+
+
+	private void Movement()
+	{
+		/*
+		2 years ago. 45 degree.
+		mirror.
+		*/
+
+		// Movement stuff
+		float newPosition = position;
+		float movement = speed * game.DeltaTime;
+
+		// Get input depending on the paddle type
+		if (paddleType == PaddleType.LEFT)
+		{
+			if(Keyboard.IsKeyPressed(Keyboard.Key.W)) newPosition -= movement;
+			if(Keyboard.IsKeyPressed(Keyboard.Key.S)) newPosition += movement;
+		}
+		else if (paddleType == PaddleType.RIGHT)
+		{
+			if(Keyboard.IsKeyPressed(Keyboard.Key.Up)) newPosition -= movement;
+			if(Keyboard.IsKeyPressed(Keyboard.Key.Down)) newPosition += movement;
+		}
+
+		// Update the position
+		if (!Collision(newPosition)) this.position = newPosition;
+		this.sprite.Position = new Vector2f(x, position);
+
+	}
+
+	private bool Collision(float newPosition)
+	{
+		// Check for if the paddle is about to hit the window
+		if ((newPosition < 0) || (newPosition + height) > game.Window.Size.Y) return true;
+
+		return false;
+	}
+
+}
+
+
+
+
+
+
+
+
+
+enum PaddleType
+{
+	LEFT,
+	RIGHT
+}
