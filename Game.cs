8fd31efce817fd2d2578235b687d66821cd8e227
MaximilianMcC <max@maximilian.co.nz>
2023-06-19 11:13:09 +1000

    Added dashed line in the middle of the screen

--- Game.cs
+++ Game.cs
@@ -49,8 +49,33 @@ class Game
 			RightPaddle.Render();
 			ball.Render();
 
+			// Draw the line down the middle
+			DrawLine();
+
 			// Show the new frame
 			Window.Display();
+		}
+	}
+
+
+
+
+	private void DrawLine()
+	{
+		// Make the rectangle line thing to draw
+		int dashCount = 51;
+		float size = (Window.Size.Y / dashCount);
+		float y = (size / 2);
+
+		RectangleShape dash = new RectangleShape(new Vector2f(size, size));
+		dash.Origin = new Vector2f((dash.Size.X / 2), ((dash.Size.Y / 2)));
+		dash.FillColor = new Color(0x17181aff);
+
+		for (int i = 0; i < dashCount; i++)
+		{
+			dash.Position = new Vector2f((Window.Size.X / 2), y);
+			Window.Draw(dash);
+			y += size * 2;
 		}
 	}
 }
