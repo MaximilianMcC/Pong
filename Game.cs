19face9e7b2114f64ac4b6b27820d506b6ac35e5
MaximilianMcC <max@maximilian.co.nz>
2023-06-19 10:02:19 +1000

    Added ball

--- Game.cs
+++ Game.cs
@@ -10,18 +10,20 @@ class Game
 	public void Run()
 	{
 		// Create the SFML window
-		Window = new RenderWindow(new VideoMode(800, 800), "Pong");
+		Window = new RenderWindow(new VideoMode(800, 800), "Pong", Styles.Close);
 		Window.SetFramerateLimit(60);
 		Window.Closed += (sender, e) => Window.Close();
 
 		// Create the delta time clock
 		Clock deltaTimeClock = new Clock();
 
+		// Create both paddles
+		Paddle leftPaddle = new Paddle(PaddleType.LEFT, this);
+		Paddle rightPaddle = new Paddle(PaddleType.RIGHT, this);
 
-		// Create both the left and right paddles
-		Paddle leftPaddle = new Paddle(PaddleType.LEFT, Color.Blue, this);
-		Paddle rightPaddle = new Paddle(PaddleType.RIGHT, Color.Red, this);
-
+		// Create the ball
+		Ball ball = new Ball(this);
+		ball.Begin();
 
 		// Game
 		while (Window.IsOpen)
@@ -31,18 +33,19 @@ class Game
 			DeltaTime = deltaTimeClock.Restart().AsSeconds();
 
 
-			// Update the paddles
+			// Update the paddles and ball
 			leftPaddle.Update();
 			rightPaddle.Update();
-
+			ball.Update();
 
 
 			// Clear the window
-			Window.Clear(Color.Magenta);
+			Window.Clear(new Color(0x202124ff));
 			
 			// Draw the paddles
 			leftPaddle.Render();
 			rightPaddle.Render();
+			ball.Render();
 
 			// Show the new frame
 			Window.Display();
